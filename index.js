3937c058a8c733d8719b07e70e3ca0bef8864840
MONICA <monik.ferrer@gmail.com>
2024-08-20 16:32:39 +0200

    Card funstion

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
+// index.js
+
+import { createCharacterCard } from "./components/card/card";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
+const card = createCharacterCard(exampleCharacter);
+cardContainer.appendChild(card);
+
+  async function fetchCharacters() {
+  try {
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character/?page=1"
+    );
+    const data = await response.json();
+    const characters = data.results;
+
+    // Vaciar el contenedor de tarjetas
+    cardContainer.innerHTML = "";
+
+    // Crear y añadir una tarjeta para cada personaje
+    characters.forEach((character) => {
+      const card = createCharacterCard(character);
+      cardContainer.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Error fetching characters:", error);
+  }
+}
+
+// Llamar a la función para obtener y mostrar los personajes
+fetchCharacters();
+
+// Task 2
+
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
